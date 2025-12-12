fb2e9e629731d1c92f00dd14fb939413aaea1112
David Welman <dwelman@xebia.com>
2025-12-12 11:49:00 +0100

    Create basic gitea container implementation (#1)

--- gitea/terraform/outputs.tf
+++ gitea/terraform/outputs.tf
@@ -25,6 +25,27 @@ output "mysql_app_username" {
 
 output "mysql_connection_string" {
   description = "MySQL connection string for applications"
-  value       = "mysql://${scaleway_rdb_user.app_user.name}:${var.mysql_password}@${scaleway_rdb_instance.mysql.endpoint_ip}:${scaleway_rdb_instance.mysql.endpoint_port}/${scaleway_rdb_database.main.name}"
+  value       = "mysql://${scaleway_rdb_user.app_user.name}:${var.mysql_app_password}@${scaleway_rdb_instance.mysql.endpoint_ip}:${scaleway_rdb_instance.mysql.endpoint_port}/${scaleway_rdb_database.main.name}"
   sensitive   = true
+}
+
+# Gitea Container Outputs
+output "gitea_url" {
+  description = "Gitea public URL"
+  value       = "https://${scaleway_container.gitea.domain_name}"
+}
+
+output "gitea_domain" {
+  description = "Gitea container domain name"
+  value       = scaleway_container.gitea.domain_name
+}
+
+output "gitea_container_id" {
+  description = "Gitea container ID"
+  value       = scaleway_container.gitea.id
+}
+
+output "gitea_namespace_id" {
+  description = "Gitea container namespace ID"
+  value       = scaleway_container_namespace.gitea.id
 }
